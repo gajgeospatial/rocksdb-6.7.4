1a98a7ce1447205fe714478b61eb1a96dafbf350
Visual Awareness Technologies and Consulting <gjohnson@vatcinc.com>
2020-04-14 19:12:36 -0400

    added windows build
    added windows build

---
+++ msvc/build_version.cc
@@ -0,0 +1,5 @@
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+#include "build_version.h"
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@5c19a441c42fe516e3b9685a7d9b30d2a9234065@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2020/04/11 18:56:45@";
+const char* rocksdb_build_compile_date = __DATE__;
---
+++ msvc/tools/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Development/op3d_active/rocksdb-6.7.4/tools
+# Build directory: C:/Development/op3d_active/rocksdb-6.7.4/msvc/tools
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
